883404dc4fe8b1036b0a28d7332419216a65a9e5
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-05-10 02:06:51 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,52 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cors from "cors";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-import * as middlewares from "./middlewares";
-import MessageResponse from "./interfaces/MessageResponse";
+import * as middlewares from './middlewares';
+import MessageResponse from './interfaces/MessageResponse';
 import {
   ClientToServerEvents,
   ServerToClientEvents,
-} from "./interfaces/ISocket";
+} from './interfaces/ISocket';
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 const httpServer = createServer(app);
 
-const io = new Server<ServerToClientEvents, ClientToServerEvents>(httpServer, {
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`${socket.id} user just connected`);
-  socket.on("disconnect", () => {
-    console.log("user just disconnected");
+  socket.on('disconnect', () => {
+    console.log('user just disconnected');
+  });
+  socket.on('update', (data) => {
+    console.log(data);
+    if (data === 'animal') {
+      socket.broadcast.emit('addAnimal', 'New animal added');
+    } else if (data === 'species') {
+      socket.broadcast.emit('addSpecies', 'New species added');
+    }
   });
 });
 
-app.get<{}, MessageResponse>("/", (req, res) => {
+app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
   });
 });
 
